addda282d23cd523d1b8df6da34b2679e681a050
Jake Salgado <5466068+jaek-s@users.noreply.github.com>
2023-11-07 23:20:48 -0700

    chapter 14

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -20,7 +20,7 @@ export type Invoice = {
   id: string; // Will be created on the database
   customer_id: string;
   amount: number; // Stored in cents
-  date: string;.
+  date: string;
   status: "pending" | "paid";
 };
 
